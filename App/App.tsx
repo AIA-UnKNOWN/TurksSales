a71ca69e05542892e086054744fe6e1656d34d40
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-01-02 19:51:06 +0800

    code refactor

--- App/App.tsx
+++ App/App.tsx
@@ -1,16 +1,5 @@
-import styles from './App.styles';
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  View,
-} from 'react-native';
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
