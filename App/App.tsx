36529f1fdf7054e37dfd8eeb0489197bcd0ac4b5
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-01-07 13:31:22 +0800

    implemented DSR page with sample data

--- App/App.tsx
+++ App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type {Node} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 // Screens
 import {
@@ -14,21 +15,23 @@ const Stack = createNativeStackNavigator();
 
 const App: () => Node = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="DSR">
-        <Stack.Screen
-          name="DSR"
-          component={DSRPage}
-          options={{
-            title: 'Daily Sales Report (DSR)',
-          }}
-        />
-        <Stack.Screen
-          name="Sales Tracker"
-          component={SalesTrackerPage}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="DSR">
+          <Stack.Screen
+            name="DSR"
+            component={DSRPage}
+            options={{
+              title: 'Daily Sales Report (DSR)',
+            }}
+          />
+          <Stack.Screen
+            name="Sales Tracker"
+            component={SalesTrackerPage}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- src/pages/DSR/DSR.tsx
+++ src/pages/DSR/DSR.tsx
@@ -1,16 +1,186 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
-  Text,
   ScrollView,
+  TextInput,
 } from 'react-native';
+import { DataTable } from 'react-native-paper';
 
 const DSR = () => {
+  const [stocks, setStocks] = useState<Array<any>>([
+    {
+      name: 'Cheese',
+      quantity: 100,
+      sold: 50,
+      leftOver: (100 - 50),
+    },
+    {
+      name: 'Hotdog',
+      quantity: 50,
+      sold: 23,
+      leftOver: (50 - 23),
+    },
+    {
+      name: 'Pita Wrap',
+      quantity: 300,
+      sold: 79,
+      leftOver: (300 - 79),
+    },
+    {
+      name: 'Tomatoes',
+      quantity: 130,
+      sold: 43,
+      leftOver: (130 - 43),
+    },
+    {
+      name: 'Cheese',
+      quantity: 100,
+      sold: 50,
+      leftOver: (100 - 50),
+    },
+    {
+      name: 'Hotdog',
+      quantity: 50,
+      sold: 23,
+      leftOver: (50 - 23),
+    },
+    {
+      name: 'Pita Wrap',
+      quantity: 300,
+      sold: 79,
+      leftOver: (300 - 79),
+    },
+    {
+      name: 'Tomatoes',
+      quantity: 130,
+      sold: 43,
+      leftOver: (130 - 43),
+    },
+    {
+      name: 'Cheese',
+      quantity: 100,
+      sold: 50,
+      leftOver: (100 - 50),
+    },
+    {
+      name: 'Hotdog',
+      quantity: 50,
+      sold: 23,
+      leftOver: (50 - 23),
+    },
+    {
+      name: 'Pita Wrap',
+      quantity: 300,
+      sold: 79,
+      leftOver: (300 - 79),
+    },
+    {
+      name: 'Tomatoes',
+      quantity: 130,
+      sold: 43,
+      leftOver: (130 - 43),
+    },
+    {
+      name: 'Cheese',
+      quantity: 100,
+      sold: 50,
+      leftOver: (100 - 50),
+    },
+    {
+      name: 'Hotdog',
+      quantity: 50,
+      sold: 23,
+      leftOver: (50 - 23),
+    },
+    {
+      name: 'Pita Wrap',
+      quantity: 300,
+      sold: 79,
+      leftOver: (300 - 79),
+    },
+    {
+      name: 'Tomatoes',
+      quantity: 130,
+      sold: 43,
+      leftOver: (130 - 43),
+    },
+  ]);
+
+  const changeValueOf = (field: string, newValue: number, itemIndex: number) : void => {
+    newValue = typeof newValue === 'string' ?
+      parseInt(newValue) :
+      newValue;
+
+    switch (field) {
+      case 'in':
+        break;
+      case 'out':
+        setStocks(stocks => 
+          stocks.map((stock, i) => {
+            if (i !== itemIndex) return stock;
+            return {
+              ...stock,
+              sold: newValue || 0,
+              leftOver: stock.quantity - newValue || 0,
+            };
+          })
+        );
+        break;
+      case 'leftOver':
+        setStocks(stocks =>
+          stocks.map((stock, i) => {
+            if (i !== itemIndex) return stock;
+            return {
+              ...stock,
+              leftOver: newValue || 0,
+              sold: stock.quantity - newValue || 0,
+            };
+          })
+        );
+        break;
+      default:
+        null;
+    }
+  }
+
   return (
     <ScrollView>
-      <View>
-        <Text>This is DSR Screen</Text>
-      </View>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Name</DataTable.Title>
+          <DataTable.Title numeric>In</DataTable.Title>
+          <DataTable.Title numeric>Out</DataTable.Title>
+          <DataTable.Title numeric>LO</DataTable.Title>
+        </DataTable.Header>
+
+        {stocks.map((stock, i) => {
+          
+          return (
+            <DataTable.Row key={i}>
+              <DataTable.Cell>{stock.name}</DataTable.Cell>
+              <DataTable.Cell numeric>{stock.quantity}</DataTable.Cell>
+              <DataTable.Cell style={{ justifyContent: 'flex-end' }}>
+                <View>
+                  <TextInput
+                    placeholder="Out"
+                    onChangeText={out => changeValueOf('out', out, i)}
+                    value={String(stock.sold)}
+                  />
+                </View>
+              </DataTable.Cell>
+              <DataTable.Cell style={{ justifyContent: 'flex-end' }}>
+                <View>
+                  <TextInput
+                    placeholder="Left Over"
+                    onChangeText={leftOver => changeValueOf('leftOver', leftOver, i)}
+                    value={String(stock.leftOver)}
+                  />
+                </View>
+              </DataTable.Cell>
+            </DataTable.Row>
+          )
+        })}
+      </DataTable>
     </ScrollView>
   );
 }
